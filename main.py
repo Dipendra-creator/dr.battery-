c81bc330bd731deedbc13f7b0beeaf5e3451b511
Dipendra-creator <66115431+Dipendra-creator@users.noreply.github.com>
2020-12-23 16:02:03 +0530

    🔋 Implemented Dr.Battery Battery Tunes 🔋

--- main.py
+++ main.py
@@ -4,17 +4,28 @@
 import psutil
 from playsound import playsound  # pip install playsound
 
+battery = psutil.sensors_battery()
+
+
+# Audio System of Battery Alert
+def alert():
+    if battery.power_plugged:
+        if battery.percent >= 90:
+            playsound('Dr.BatteryTunes/batery_full_capacity.mp3')
+        elif 60 < battery.percent < 80:
+            playsound('Dr.BatteryTunes/battery_low.mp3')
+    else:
+        if battery.percent <= 20:
+            playsound('Dr.BatteryTunes/battery_at_20.mp3')
+
 
 # Function which can Check Battery Percentage
 def checkBatteryPercentage():
-    battery = psutil.sensors_battery()
     plugged = battery.power_plugged
     percent = str(battery.percent)
     plugged = "Plugged In" if plugged else "Not Plugged In"
     print(percent + '% | ' + plugged)
-    plug = battery.power_plugged
-    if plug == True:
-        playsound("D:/python/dr.battery-/Dr.Battery tunes/batery_full_capacity.mp3")
+    alert()
 
 
 # Running Function
